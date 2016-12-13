2201962b5ae4208f09a995fd3539d3b38de0e5d1
David Flick <davidflick@Davids-MacBook-Pro.local>
2016-12-13 14:24:49 -0500

    minor updates may not last

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     console.log('test');
 });
+
+
+
+const result = $('#search').val();
+var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + result + '&format=json&callback=wikiCallback';
