543a3ce191c83b26661bb3969f0486dc59dcea69
Hazumi <shirohazumi@openmailbox.org>
2016-12-14 04:03:06 -0500

    basic styling and basic loop for data

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const app = {
   cacheDom: function() {
     this.$form = $('#form');
     this.$search = $('#search');
+    this.$links = $('#links');
   },
   bindEvents: function() {
     this.$form.submit(this.renderData);
@@ -16,20 +17,24 @@ const app = {
     let searchInput = app.$search.val();
     let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchInput}&format=json&callback=wikiCallback`;
     app.$search.val('');
-
     $.ajax({
       url: url,
       dataType: 'jsonp',
       type: 'POST',
       success: function(data) {
+
         console.log(data);
+
+        for (var i = 0; i < data.length; i++) {
+          let title = data[1][i];
+          let summary = data[2][i].substring(0, 40);
+          let link = data[3][i];
+        }
+
       }
-
     });
-
-  return false;
-  },
-
+    return false;
+  }
 };
 
 app.init();
