1ea97e199ee8587cb03284185ca296d8799c51f7
Hazumi <shirohazumi@openmailbox.org>
2016-12-13 23:56:53 -0500

    setup form submit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const $ = require('jquery');
 
-console.log('test');
+const loadData = () => {
+  console.log('test');
+  return false;
+};
 
+$('#form').submit(loadData);
 
-
-
-const result = $('#search').val();
-var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + result + '&format=json&callback=wikiCallback';
+// const result = $('#search').val();
+// var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + result + '&format=json&callback=wikiCallback';
